0baf48d177cf91456f002906466e40b649d57c57
Benjamin Horns <benhorns@gmail.com>
2019-12-26 20:48:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(deliLine){
 
 function currentLine (line){
   while (deliLine.length > 0){
-    return "The line is currently:"
+    return "The line is currently:" + line +
   }
     return "The line is currently empty."
 }
