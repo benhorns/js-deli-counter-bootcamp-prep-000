93fd8951ef8366e500b8175f3fb4c8835c8b76b9
Benjamin Horns <benhorns@gmail.com>
2019-12-26 20:29:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function takeANumber (katzDeli, name){
+  katzDeli.push (name)
+  return "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
+}
+
+function nowServing(deliLine){
+  return "Currently serving " + deliLine
+   // do {("Now Serivng" +deliLine.shift())};
+   // while (deliLine > 0);
+   // return "There is nobody waiting to be served!";
+}
