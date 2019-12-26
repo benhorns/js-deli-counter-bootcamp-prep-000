ff41b70f723ce26aaa4d95b8d441c6bd1e25c641
Benjamin Horns <benhorns@gmail.com>
2019-12-26 21:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function currentLine (line){
 for (var i = 0; i < line.length; i++ )  {
  newarray.push (" " + (i+1) + ". " + line[i])
   }
+  if (line !== [] ){
   return "The line is currently:" + newarray
+} else {
   return "The line is currently empty."
+}
 }
