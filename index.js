a28c877531992b74a6d5b5f31f14027e99bfb6c1
Benjamin Horns <benhorns@gmail.com>
2019-12-26 20:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber (katzDeli, name){
 
 function nowServing(deliLine){
   do {return "Currently serving " + deliLine.shift() + "."}
-  while (deliLine > 0)
+  while (deliLine < 0)
   return "There is nobody waiting to be served!"
    // do {("Now Serivng" +deliLine.shift())};
    // while (deliLine > 0);
